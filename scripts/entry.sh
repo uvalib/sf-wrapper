5196304b96facd1dab82fab22594e9052f1dea03
UVADave <dpg3k@virginia.edu>
2017-02-27 14:07:15 -0500

    Add java options to set memory pool size to 1GB

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,6 +1,9 @@
 # ensure encoding uses UTF-8`
 export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF8"
 
+# configure memory pool
+export _JAVA_OPTIONS="-Xms1024m -Xmx1024m"
+
 # update the path as necessary
 export PATH=$PATH:/usr/local/bundle/bin
 
