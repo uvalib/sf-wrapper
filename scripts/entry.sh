88f0712e00b44c37a84083c65fb695fd15023a9b
UVADave <dpg3k@virginia.edu>
2017-03-10 11:15:28 -0500

    Update SOLR heap memory and make the SOLR OOM killer script executable

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -2,7 +2,7 @@
 export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF8"
 
 # configure SOLR memory pool
-export SOLR_HEAP=1g
+export SOLR_HEAP=2048m
 
 # update the path as necessary
 export PATH=$PATH:/usr/local/bundle/bin
@@ -13,4 +13,12 @@ sleep 15
 
 # start Fedora
 fcrepo_wrapper &
+
+# some housekeeping
+oom_script=$APP_HOME/tmp/solr/bin/oom_solr.sh
+if [ -f $oom_script ]; then
+   chmod +x $oom_script
+fi
+
+# sleep forever
 sleep 999d
